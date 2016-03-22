0d9bdcdfe2a46caff8b215d6d12065a69bd82f46
hans123456 <h.g.n.chua@gmail.com>
2016-03-22 11:28:27 +0800

    rotation for camera and ball gravity and four walls

---
+++ Assets/MovementScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+
+	public float speed;
+	public Rigidbody rigidBody;
+	private float horizontalSpeed = 0;
+	private float verticalSpeed = 0;
+	private float limit = 30;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float moveHorizontal = Input.GetKey (KeyCode.LeftArrow) ? -1 : Input.GetKey (KeyCode.RightArrow) ? 1 : 0;
+		float moveVertical = Input.GetKey (KeyCode.UpArrow) ? 1 : Input.GetKey (KeyCode.DownArrow) ? -1 : 0;
+
+		if (horizontalSpeed + moveHorizontal > -limit && horizontalSpeed + moveHorizontal < limit) {
+			horizontalSpeed += moveHorizontal;
+		} 
+
+		if (verticalSpeed + moveVertical > -limit && verticalSpeed + moveVertical < limit) {
+			verticalSpeed += moveVertical;
+		}
+
+		Physics.gravity = new Vector3 (horizontalSpeed * Time.deltaTime * speed, -9.8f, verticalSpeed * Time.deltaTime * speed);
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Physics.gravity = new Vector3 (0, -9.8f, 0);
+			rigidBody.velocity = Vector3.zero;
+			rigidBody.angularVelocity = Vector3.zero;
+			verticalSpeed = 0;
+			horizontalSpeed = 0;
+		}
+
+	}
+
+}
---
+++ Assets/TopViewScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TopViewScript : MonoBehaviour {
+
+	private Quaternion defaultRotation;
+	private Vector3 defaultPosition;
+	public float speedOfRotation;
+
+	private Vector3 targetPosition;
+	public GameObject targetGameObject;
+
+	// Use this for initialization
+	void Start () {
+		defaultRotation = transform.localRotation;
+		defaultPosition = transform.position;
+		targetPosition = targetGameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		float moveZ = Input.GetKey (KeyCode.LeftArrow) ? -1 : Input.GetKey (KeyCode.RightArrow) ? 1 : 0;
+		float moveX = Input.GetKey (KeyCode.UpArrow) ? -1 : Input.GetKey (KeyCode.DownArrow) ? 1 : 0;
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			transform.position = defaultPosition;
+			transform.localRotation = defaultRotation;
+		}
+
+		transform.RotateAround (targetPosition, new Vector3(moveX, 0, moveZ), Time.deltaTime * speedOfRotation);
+
+	}
+}
