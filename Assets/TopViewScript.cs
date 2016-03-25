3f81b05f14911646b28a8ff0bf990bb055e87ea2
hans123456 <h.g.n.chua@gmail.com>
2016-03-26 00:23:38 +0800

    maze generation

--- Assets/TopViewScript.cs
+++ Assets/TopViewScript.cs
@@ -8,6 +8,9 @@ public class TopViewScript : MonoBehaviour {
 	public float speedOfRotation;
 
 	private Vector3 targetPosition;
+	private float max = 20;
+	private float accuX = 0;
+	private float accuZ = 0;
 	public GameObject targetGameObject;
 
 	// Use this for initialization
@@ -26,6 +29,19 @@ public class TopViewScript : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			transform.position = defaultPosition;
 			transform.localRotation = defaultRotation;
+			accuX = accuZ = 0;
+		}
+
+		if (moveX + accuX < max && moveX + accuX > -max) {
+			accuX += moveX;
+		} else {
+			moveX = 0;
+		}
+
+		if (moveZ + accuZ < max && moveZ + accuZ > -max) {
+			accuZ += moveZ;
+		} else {
+			moveZ = 0;
 		}
 
 		transform.RotateAround (targetPosition, new Vector3(moveX, 0, moveZ), Time.deltaTime * speedOfRotation);
